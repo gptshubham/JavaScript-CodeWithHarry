49960ae48accf53a70ad358bac2b2a1b4d85f87b
gptshubham <shubhamca76@gmail.com>
2023-12-26 22:22:46 +0530

    l6 on Operators completes

---
+++ T6-Operators-and-Expressions/index.js
@@ -0,0 +1,28 @@
+// Operators in JavaScript
+// Arithmetic 
+let a = 45;
+let b = 4;
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// increment
+a++;
+console.log(a);
+// decrement
+b--;
+console.log(b);
+
+// Asignment Operators - piece of cake
+// = , += , -= , *= , /= , %= , **=
+
+// Comparision Operators
+// == , != , === , !== , > , < , >= , <= , ?
+
+console.log(6 == "6")
+console.log(6 === "6")
+
+// Logical Operators
+// && --> and
+// || --> or
+// !  --> not

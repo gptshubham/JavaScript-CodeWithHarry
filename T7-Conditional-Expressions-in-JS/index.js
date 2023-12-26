abdfc1baef86adedb3b1e0fb6e7f78e425ede432
gptshubham <shubhamca76@gmail.com>
2023-12-26 22:24:01 +0530

    lec7 on conditionals completes

---
+++ T7-Conditional-Expressions-in-JS/index.js
@@ -0,0 +1,31 @@
+/*
+// if-else
+let age = 17;
+if (age<18){
+    console.log("You cannot drive.");
+} 
+else {
+    console.log("You can drive.");
+}
+// else if 
+let Drunk = true;
+if (age<18){
+    console.log("You cannot drive.");
+} 
+else if (Drunk){
+    console.log("You can not drive");
+} 
+else {
+    console.log("You can drive");
+}
+*/
+
+/*
+// Switch-case Statements --> Homework
+*/
+
+
+// Ternary Operator
+// let age = 17
+let age = 19;
+console.log("you can", age<18? "not drive":"drive")

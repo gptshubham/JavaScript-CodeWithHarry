498470a5ca193e361032e82d63b5d3bd125e5d48
gptshubham <shubhamca76@gmail.com>
2023-12-27 15:50:38 +0530

    lec 10 on while loop completes

---
+++ T10-While-Loops-in-JS/index.js
@@ -0,0 +1,32 @@
+// while loop
+/*
+let i = 0;
+while (i<11) {
+    console.log(i);
+    i++;
+}
+*/
+
+
+// do while loop
+/*
+const n = 11;
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while(i<n);
+*/
+
+
+// while loop vs do while loop 
+const n = 10;
+let i = 11;
+// while (i<n){
+//     console.log(i);
+// }
+
+do {
+    console.log(i);
+}
+while(i<n)

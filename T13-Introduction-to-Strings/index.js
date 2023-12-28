ec972981e9f63bff93ab223565c5ccb2185946cf
gptshubham <shubhamca76@gmail.com>
2023-12-28 14:59:07 +0530

    let 13 on introduction to strings completes

---
+++ T13-Introduction-to-Strings/index.js
@@ -0,0 +1,28 @@
+// Strings
+const firstName = "Shubham";
+// console.log(firstName);
+// console.log(firstName.length);
+// console.log(typeof firstName);
+const friend = 'Mayank';
+// console.log(friend);
+// console.log(friend.length);
+// console.log(typeof friend);
+/*
+for (i of firstName){
+    console.log(i);
+}
+*/
+
+
+// Template Literals
+const age = 22;
+const gender = "male";
+// console.log("my name is "+ firstName + " and I am a " + age + " years old " + gender + ".")
+// console.log(`my name is ${firstName} and I am a ${age} years old ${gender}.`)  // template literal
+
+
+// Escape Sequence Character
+// \
+console.log('Shubham is Abhishek\'s brother and Mayank\'s friend');
+// \n
+console.log('Shubham\nAbhishek\nMayank');

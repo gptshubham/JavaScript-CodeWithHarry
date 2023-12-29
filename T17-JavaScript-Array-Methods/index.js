11d752047cb564035463d0b5f05b73011a9ae036
gptshubham <shubhamca76@gmail.com>
2023-12-29 14:32:45 +0530

    lec17 on array methods in JS completes

---
+++ T17-JavaScript-Array-Methods/index.js
@@ -0,0 +1,53 @@
+// JS Array Methods
+const num = [1,2,3,34,4];
+
+// .toString()
+/*
+const b = num.toString();
+console.log(typeof b);
+*/
+
+// .join()
+/*
+const c = num.join("-");
+console.log(c);
+console.log(typeof c);
+
+const fullName = "shubham kumar gupta";
+console.log(fullName);
+const fullNameArray = fullName.split(" ");
+console.log(fullNameArray);
+const fullNameString = fullNameArray.join("_");
+console.log(fullNameString);
+*/
+
+// .pop()
+/*
+console.log(num);
+let r = num.pop();         // pop() returns the popped value.
+console.log(num);
+console.log(r);
+*/
+
+// .push()
+/*
+console.log(num);
+let p = num.push(56);      // push() returns the length of new array.
+console.log(num);
+console.log(p);
+*/
+
+// .shift()
+/*
+console.log(num);
+let s = num.shift();        // shift() returns the shifted element
+console.log(num)
+console.log(s);
+*/
+
+// .unshift()
+
+console.log(num);
+let u = num.unshift(12);    // unshift() returns the length of new array
+console.log(num);
+console.log(u);

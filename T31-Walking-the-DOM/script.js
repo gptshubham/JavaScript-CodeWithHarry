067f2138428de10dffb7ac32deb23d3840d91af3
gptshubham <shubhamca76@gmail.com>
2024-01-04 16:06:15 +0530

    lec 31 on Walking the DOM completes

---
+++ T31-Walking-the-DOM/script.js
@@ -0,0 +1,21 @@
+document.getElementsByClassName("outer-div")[2].style.color = "red";
+
+document.getElementById("ol-third").style.color = "green";
+'green'
+
+document.getElementsByClassName("outer-div")[0].style.background = "yellow";
+document.getElementsByClassName("outer-div")[0].style.color = "forestgreen"
+/*
+document.head
+document.body
+document.title
+document.documentElement
+*/
+
+// changing the title using JavaScript
+document.title = "Vo Badal Gaya";    // the content of title element of html remains the same just title of the page changes as per JavaScript code.
+
+// changing style using DOM
+document.body.style.background = "lightgray";
+// document.h1.style.background = "red"; // doesn't work like this use .getElemets instead.
+// document.body.h1.style.color = "white";  //doesn't work like this use .getElemets instead.
